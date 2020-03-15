008e823c1a1004cec44cf5beaf3c58dc72c17c69
Daniel Berthereau <Daniel.github@Berthereau.net>
2020-03-16 00:00:00 +0100

    Replaced option by an automatic url.

--- Module.php
+++ Module.php
@@ -2,6 +2,8 @@
 namespace IiifSearch;
 
 use Omeka\Module\AbstractModule;
+use Zend\EventManager\Event;
+use Zend\EventManager\SharedEventManagerInterface;
 use Zend\Mvc\MvcEvent;
 
 class Module extends AbstractModule
@@ -17,5 +19,55 @@ class Module extends AbstractModule
 
         $acl = $this->getServiceLocator()->get('Omeka\Acl');
         $acl->allow(null, 'IiifSearch\Controller\Search');
+    }
+
+    public function attachListeners(SharedEventManagerInterface $sharedEventManager)
+    {
+        $sharedEventManager->attach(
+            '*',
+            'iiifserver.manifest',
+            [$this, 'handleIiifServerManifest']
+        );
+    }
+
+    public function handleIiifServerManifest(Event $event)
+    {
+        $type = $event->getParam('type');
+        if ($type !== 'item') {
+            return;
+        }
+
+        $resource = $event->getParam('resource');
+
+        // Checking if resource has at least an XML file that will allow search.
+        $searchServiceAvailable = false;
+        $searchMediaTypes = [
+            'application/xml',
+            'text/xml',
+            'application/vnd.pdf2xml+xml',
+        ];
+        foreach ($resource->media() as $media) {
+            $mediaType = $media->mediaType();
+            if (in_array($mediaType, $searchMediaTypes)) {
+                $searchServiceAvailable = true;
+                break;
+            }
+        }
+        if (!$searchServiceAvailable) {
+            return;
+        }
+
+        $urlHelper = $this->getServiceLocator()->get('ViewHelperManager')->get('url');
+
+        $manifest = $event->getParam('manifest');
+
+        $manifest['service'][] = [
+            '@context' => 'http://iiif.io/api/search/0/context.json',
+            '@id' => $urlHelper('iiifsearch', ['id' => $resource->id()], ['force_canonical' => true]),
+            'profile' => 'http://iiif.io/api/search/0/search',
+            'label' => 'Search within this manifest', // @translate
+        ];
+
+        $event->setParam('manifest', $manifest);
     }
 }
