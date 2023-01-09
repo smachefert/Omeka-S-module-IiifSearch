60b59e34f414fa120d8cb5aab1a30fc7d0cb6f5b
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-01-09 00:00:00 +0000

    Cleaned code.

--- data/scripts/upgrade.php
+++ data/scripts/upgrade.php
@@ -6,23 +6,22 @@ use Omeka\Stdlib\Message;
 
 /**
  * @var Module $this
- * @var \Laminas\ServiceManager\ServiceLocatorInterface $serviceLocator
+ * @var \Laminas\ServiceManager\ServiceLocatorInterface $services
  * @var string $newVersion
  * @var string $oldVersion
  *
+ * @var \Omeka\Api\Manager $api
+ * @var \Omeka\Settings\Settings $settings
  * @var \Doctrine\DBAL\Connection $connection
  * @var \Doctrine\ORM\EntityManager $entityManager
- * @var \Omeka\Api\Manager $api
  * @var \Omeka\Mvc\Controller\Plugin\Messenger $messenger
  */
-$services = $serviceLocator;
 $plugins = $services->get('ControllerPluginManager');
-// $api = $plugins->get('api');
-// $config = require dirname(dirname(__DIR__)) . '/config/module.config.php';
+$api = $plugins->get('api');
 $settings = $services->get('Omeka\Settings');
-// $connection = $services->get('Omeka\Connection');
-// $entityManager = $services->get('Omeka\EntityManager');
+$connection = $services->get('Omeka\Connection');
 $messenger = $plugins->get('messenger');
+$entityManager = $services->get('Omeka\EntityManager');
 
 if (version_compare($oldVersion, '1.1.0', '<')) {
     $settings->delete('iiifserver_manifest_service_iiifsearch');
