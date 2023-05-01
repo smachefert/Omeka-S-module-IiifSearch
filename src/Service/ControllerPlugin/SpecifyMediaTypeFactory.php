a99f25d36f4afa49f37e51ccb56ba8dd3b6da2db
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-05-01 00:00:00 +0000

    Improved check of media type for xml files.

--- src/Service/ControllerPlugin/SpecifyMediaTypeFactory.php
+++ src/Service/ControllerPlugin/SpecifyMediaTypeFactory.php
@@ -10,9 +10,12 @@ class SpecifyMediaTypeFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
     {
+        $config = $services->get('Config');
         $mediaTypesIdentifiers = require_once dirname(__DIR__, 3) . '/data/media-types/media-type-identifiers.php';
         return new SpecifyMediaType(
             $services->get('Omeka\Logger'),
+            $services->get('Omeka\Connection'),
+            $config['file_store']['local']['base_path'] ?: (OMEKA_PATH . '/files'),
             $mediaTypesIdentifiers
         );
     }
